8c0bc4b6e771af7458d622baf8844f8fb85db977
mohamed-elhaj <mohamedelhaj@gmail.com>
2021-03-16 20:55:53 +0200

    All the links are clickable, random page is working

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("random", views.random, name="random_page"),
+    path("<str:entry>", views.entry, name="entry")
 ]
+
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from markdown2 import Markdown
+from random import randint
 
 from . import util
 
@@ -7,4 +9,29 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+
+def entry(request, entry):
+
+    markdowner = Markdown()
+    page = util.get_entry(entry)
+    if page is None:
+        return render(request, "encyclopedia/error.html", {
+            "name": entry
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": entry,
+            "content": markdowner.convert(page)
+        })
+
+def random(request):
+    markdowner = Markdown()
+    entries = util.list_entries()
+    random_page = entries[randint(0, len(entries) - 1)]
+    random_page = util.get_entry(random_page)
+    return render(request, "encyclopedia/entry.html", {
+        "title": "random_page",
+        "content": markdowner.convert(random_page)
+    })        
 
